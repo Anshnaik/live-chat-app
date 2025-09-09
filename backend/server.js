4a29cf267c0e6e51d99599577ca4e105cbb1bdcb
Ansh Naik <nansh5841@gmail.com>
2025-09-09 13:01:17 +0530

    dont know what i am doing

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,12 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Serve frontend build
 const frontendPath = path.join(__dirname, "../frontend/dist");
 app.use(express.static(frontendPath));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
 });
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(frontendPath, "index.html"));
+// });
 
 // Multer storage
 const storage = multer.diskStorage({
